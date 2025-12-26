7088145fc6fddaa55d68a29edd48006adccfa9f3
Haseeb-Persistent <haseebpersistent@gmail.com>
2025-12-26 17:13:06 +0500

    ok

--- src/app/core/Services/catalog-service.service.ts
+++ src/app/core/Services/catalog-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BrandResDto, CatagoryResDto, ProductFilter, ProductPaginationRes, ProductResDto } from '../Models/catalog';
 import { ResponseDto } from '../Models/ResponseDto';
+import { environment } from '../enviroment/enviroment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,26 @@ export class CatalogService {
 
 constructor(private http:HttpClient) { }
 
-  getCategories(){
-    return this.http.get<ResponseDto<CatagoryResDto[]>>('/Catalog/category/getall')
-  }
-  getBrands(){
-    return this.http.get<ResponseDto<BrandResDto[]>>('/Catalog/brand/getall')
-  }
+getCategories(){
+  return this.http.get<ResponseDto<CatagoryResDto[]>>(
+    `${environment.baseApi}Catalog/category/getall`
+  );
+}
+
+getBrands(){
+  return this.http.get<ResponseDto<BrandResDto[]>>(
+    `${environment.baseApi}Catalog/brand/getall`
+  );
+}
+
 getAllProducts(filter: ProductFilter) {
   return this.http.post<ResponseDto<ProductPaginationRes>>(
-    '/Catalog/product/getall',
+    `${environment.baseApi}Catalog/product/getall`,
     filter
   );
-
-
 }
+
+
+
 
 }
--- src/app/core/enviroment/enviroment.ts
+++ src/app/core/enviroment/enviroment.ts
@@ -1,11 +1,11 @@
-export const environment = {
-  baseApi : 'http://localhost:5106/api',
-  imageBaseApi: 'http://localhost:5106'
-};
-
-
-
 // export const environment = {
-//   baseApi: 'http://haseebapieshop.runasp.net/api/',
-//   imageBaseApi: 'http://haseebapieshop.runasp.net'
+//   baseApi : 'http://localhost:5106/api',
+//   imageBaseApi: 'http://localhost:5106'
 // };
+
+
+
+export const environment = {
+  baseApi: 'http://haseebapieshop.runasp.net/api/',
+  imageBaseApi: 'http://haseebapieshop.runasp.net'
+};
